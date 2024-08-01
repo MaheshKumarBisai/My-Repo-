eb88b5cf1b5f9f6ba0e2163651cab40c27b23bb9
MaheshKumarBisai <maheshbisai36@gmail.com>
2024-08-01 22:09:48 +0530

    Added a loop with condition

--- sample.py
+++ sample.py
@@ -1,4 +1,14 @@
+
+# Loop for printing number iteration 
 for i in range (1,10):
     if i == 6:
         continue
     print(i)
+
+# Loop with condition statemnt 
+some_fruits = ["apple","banana","cranberry"]
+if "banana" in some_fruits:
+    for some_fruits in some_fruits:
+        print(some_fruits)
+else:
+    print("There is no fruits")
